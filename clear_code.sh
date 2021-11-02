0db72fb2f238451200c60d660944ec9036ab7353
zhipeng.yin <english126paopao@163.com>
2021-11-02 15:38:20 +0800

    commit_message

--- clear_code.sh
+++ clear_code.sh
@@ -5,3 +5,6 @@ rm -rf !(mkdocs.yml|README.md|docs|site|.git|clear_code.sh|requirements.txt)
 pip freeze > requirements.txt
 mkdocs build
 mv site/* ./
+git add . -A 
+git commit -m "commit_message"
+git push
